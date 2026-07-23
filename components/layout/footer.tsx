202269bc8a40b4c5c4873bd9fb928380c16ab29e
Dinesh Mardi <dineshmardi9523@gmail.com>
2026-07-23 20:14:47 +0530

    fix: normalize component filenames for Linux deployment

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,4 +1,4 @@
-import Container from "@/components/ui/Container";
+import Container from "@/components/ui/container";
 import { Logo } from "@/components/brand/logo";
 
 export default function Footer() {
